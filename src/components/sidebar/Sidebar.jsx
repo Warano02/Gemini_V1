134eb6c00abd2c1432e9487cfb53fcf6bbd16d06
Warano02 <carineteoi@gmail.com>
2025-05-17 09:29:47 +0100

    start project

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,38 @@
+import { assets } from "../../assets/assets"
+import "./sidebar.css"
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <img src={assets.menu_icon} className="menu" alt="" />
+                <div className="new-chat">
+                    <img src={assets.plus_icon} alt="" />
+                    <p>new chat</p>
+                </div>
+                <div className="recent">
+                    <p className="recent-title">Recent</p>
+                    <div className="recent-entry">
+                        <img src={assets.menu_icon} alt="" />
+                        <p>What is react ...</p>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="bottom-item recent-entry">
+                    <img src={assets.question_icon} alt="" />
+                    <p>Help</p>
+                </div>
+                <div className="bottom-item recent-entry">
+                    <img src={assets.history_icon} alt="" />
+                    <p>Activity</p>
+                </div>
+                <div className="bottom-item recent-entry">
+                    <img src={assets.setting_icon} alt="" />
+                    <p>Settings</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
